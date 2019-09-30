7ec12040260addbc1d68fc6d383ca56128e0baf5
Sibbons Shrestha <Sibbonsss@gmail.com>
2019-09-30 00:11:56 -0700

    anchor tag no longer changes URL

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 
-
 burger.addEventListener('click', () => {
 
     nav.classList.toggle('toggle')
-
-
 });
+
+function jump(h) {
+    var url = location.href;               //Save down the URL without hash.
+    location.href = "#" + h;                 //Go to the target element.
+    history.replaceState(null, null, url);   //Don't like hashes. Changing it back.
+}
